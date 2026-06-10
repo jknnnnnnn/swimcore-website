e86ac8c3c38c27487d2c51bdfbf033e849189d1b
Ytu Qete <ytuqete@gmail.com>
2026-06-10 12:48:16 +0200

    iguessthatsssssssssssssss

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -5,7 +5,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
     {
       url: "https://www.swimcore.pl",
       lastModified: new Date(),
-      changeFrequency: "monthly",
+      changeFrequency: "weekly",
       priority: 1,
     },
   ];
