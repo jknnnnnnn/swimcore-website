2322b2b2532471e3fd5943b5a3721a2a2a5a1daf
Ytu Qete <ytuqete@gmail.com>
2026-06-13 11:34:08 +0200

    Fix redirect loop

--- next.config.mjs
+++ next.config.mjs
@@ -3,64 +3,6 @@ const nextConfig = {
   reactStrictMode: true,
   trailingSlash: true,
   skipTrailingSlashRedirect: true,
-  async redirects() {
-    return [
-      {
-        source: "/",
-        has: [
-          {
-            type: "host",
-            value: "www.swimcore.pl",
-          },
-        ],
-        destination: "https://swimcore.pl/",
-        statusCode: 301,
-      },
-      {
-        source: "/:path*",
-        has: [
-          {
-            type: "host",
-            value: "www.swimcore.pl",
-          },
-        ],
-        destination: "https://swimcore.pl/:path*",
-        statusCode: 301,
-      },
-      {
-        source: "/",
-        has: [
-          {
-            type: "host",
-            value: "swimcore.pl",
-          },
-          {
-            type: "header",
-            key: "x-forwarded-proto",
-            value: "http",
-          },
-        ],
-        destination: "https://swimcore.pl/",
-        statusCode: 301,
-      },
-      {
-        source: "/:path*",
-        has: [
-          {
-            type: "host",
-            value: "swimcore.pl",
-          },
-          {
-            type: "header",
-            key: "x-forwarded-proto",
-            value: "http",
-          },
-        ],
-        destination: "https://swimcore.pl/:path*",
-        statusCode: 301,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
